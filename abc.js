54372f54193775f86dee416d7a596c5a7191975e
karamdeep.singh <karamdeep.singh@KSingh-MacPro.local>
2019-04-15 01:36:27 -0400

    updated cred

--- abc.js
+++ abc.js
@@ -4,5 +4,12 @@ console.log("hello",a);
 var n=process.env.name;
 var url=process.env.url;
 
+var username=process.env.usernameVar;
+var password=process.env.passwordVar;
+
 console.log("environment variable name",n);
 console.log("environment variable url",url);
+
+
+console.log("username is",username);
+console.log("password is",password);
